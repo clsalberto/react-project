fd82f659cfcca796b2a2968692a05a587917c52e
Carlos Alberto <clsalberto@outlook.com>
2020-06-25 21:10:59 -0300

    refactor: added code refactoring, global styles and configuration files

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,17 +1,40 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  outline: 0;
-  box-sizing: border-box;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
 
-body {
-  background: ${(props) => props.theme.colors.background};
-  font-family: sans-serif;
-  font-size: 14px;
-  color: ${(props) => props.theme.colors.text};
-}
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors.text};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Muli', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
 `;
